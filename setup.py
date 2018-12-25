82c4beddafcb0615edfc5d5e3bc27774a8507343
Andy Tzeng <andy.tzeng@soocii.me>
2018-12-25 14:17:14 +0800

    optionally install python2-secrets for older runtime

--- setup.py
+++ setup.py
@@ -36,7 +36,11 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['python2-secrets', 'cryptography', 'jsonschema'],
+    install_requires=[
+        'python2-secrets;python_version<"3.6"',
+        'cryptography',
+        'jsonschema',
+    ],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
